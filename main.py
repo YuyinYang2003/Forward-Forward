4f72b4f8ab8017620139a12e815b860715a0c7e1
YuyinYang <120449838+YuyinYang2003@users.noreply.github.com>
2023-03-07 03:47:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from src import utils
 
 def train(opt, model, optimizer):
     start_time = time.time()
-    train_loader = utils.get_data(opt, "train")
-    num_steps_per_epoch = len(train_loader)
+    train_loader = utils.get_data(opt, "train") #训练的dataloader
+    num_steps_per_epoch = len(train_loader) #批次数
 
     for epoch in range(opt.training.epochs):
         train_results = defaultdict(float)
-        optimizer = utils.update_learning_rate(optimizer, opt, epoch)
+        optimizer = utils.update_learning_rate(optimizer, opt, epoch)   #每个epoch都cooldown lr
 
         for inputs, labels in train_loader:
             inputs, labels = utils.preprocess_inputs(opt, inputs, labels)
@@ -29,13 +29,14 @@ def train(opt, model, optimizer):
 
             train_results = utils.log_results(
                 train_results, scalar_outputs, num_steps_per_epoch
-            )
+            )   #更新这次迭代的train result
 
         utils.print_results("train", time.time() - start_time, train_results, epoch)
         start_time = time.time()
 
         # Validate.
-        if epoch % opt.training.val_idx == 0 and opt.training.val_idx != -1:
+        if epoch % opt.training.val_idx == 0 and opt.training.val_idx != -1:    
+            #-1: validate only once training has finished; n: validate every n epochs
             validate_or_test(opt, model, "val", epoch=epoch)
 
     return model
@@ -75,6 +76,10 @@ def my_main(opt: DictConfig) -> None:
     if opt.training.final_test:
         validate_or_test(opt, model, "test")
 
+@hydra.main(config_path=".", config_name="config", version_base=None)
+def show_parameters(opt: DictConfig) -> None:
+    opt = utils.parse_args(opt)
+    utils.show_model_parameters(opt)
 
 if __name__ == "__main__":
     my_main()
