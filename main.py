a48d679ad4992e19381dd683cd8a82f3813048b9
YuyinYang <120449838+YuyinYang2003@users.noreply.github.com>
2023-03-15 11:05:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import time
 from collections import defaultdict
 
 import hydra
+import wandb
 import torch
+import omegaconf
 from omegaconf import DictConfig
 
 from src import utils
 
 
 def train(opt, model, optimizer):
+    '''epoch -> batch -> layer, 完成所有训练, 返回参数训练完毕的 model.'''
     start_time = time.time()
-    train_loader = utils.get_data(opt, "train") #训练的dataloader
-    num_steps_per_epoch = len(train_loader) #批次数
+    train_loader = utils.get_data(opt, "train")
+    num_steps_per_epoch = len(train_loader) # 即 num_batches, 总的批次数目.
 
     for epoch in range(opt.training.epochs):
         train_results = defaultdict(float)
-        optimizer = utils.update_learning_rate(optimizer, opt, epoch)   #每个epoch都cooldown lr
+        optimizer = utils.update_learning_rate(optimizer, opt, epoch)
 
         for inputs, labels in train_loader:
             inputs, labels = utils.preprocess_inputs(opt, inputs, labels)
@@ -23,20 +26,21 @@ def train(opt, model, optimizer):
             optimizer.zero_grad()
 
             scalar_outputs = model(inputs, labels)
-            scalar_outputs["Loss"].backward()
+            scalar_outputs["Loss"].backward()   # 3层 ff_loss 与 线性分类器的 classification_loss 加在一起,
+            # .backward() 时相当于对每层的 loss 分别求导, 不需要多层的反向传播.
 
             optimizer.step()
 
+            # 把当前这个 batch 的训练结果更新到总的 train_results 中, num_batches 决定了这次结果占总结果的权重.
             train_results = utils.log_results(
                 train_results, scalar_outputs, num_steps_per_epoch
-            )   #更新这次迭代的train result
+            )
 
         utils.print_results("train", time.time() - start_time, train_results, epoch)
         start_time = time.time()
 
         # Validate.
-        if epoch % opt.training.val_idx == 0 and opt.training.val_idx != -1:    
-            #-1: validate only once training has finished; n: validate every n epochs
+        if epoch % opt.training.val_idx == 0 and opt.training.val_idx != -1:
             validate_or_test(opt, model, "val", epoch=epoch)
 
     return model
@@ -49,7 +53,7 @@ def validate_or_test(opt, model, partition, epoch=None):
     data_loader = utils.get_data(opt, partition)
     num_steps_per_epoch = len(data_loader)
 
-    model.eval()
+    model.eval()    # model 默认是在 train() 状态, 因此上面的 train 函数中不需要 model.train().
     print(partition)
     with torch.no_grad():
         for inputs, labels in data_loader:
@@ -63,12 +67,18 @@ def validate_or_test(opt, model, partition, epoch=None):
             )
 
     utils.print_results(partition, time.time() - test_time, test_results, epoch=epoch)
-    model.train()
+    model.train()   # test 结束后把 model 状态恢复到默认的 train().
 
 
-@hydra.main(config_path=".", config_name="config", version_base=None)
+@hydra.main(version_base=None, config_path="configs/", config_name="defaults")
 def my_main(opt: DictConfig) -> None:
     opt = utils.parse_args(opt)
+
+    wandb_cfg = omegaconf.OmegaConf.to_container(
+        opt, resolve=True, throw_on_missing=True
+    )
+    wandb.init(entity=opt.wandb.setup.entity, project=opt.wandb.setup.project, config=wandb_cfg)
+
     model, optimizer = utils.get_model_and_optimizer(opt)
     model = train(opt, model, optimizer)
     validate_or_test(opt, model, "val")
@@ -76,10 +86,16 @@ def my_main(opt: DictConfig) -> None:
     if opt.training.final_test:
         validate_or_test(opt, model, "test")
 
-@hydra.main(config_path=".", config_name="config", version_base=None)
+    wandb.finish()
+
+
+@hydra.main(version_base=None, config_path="configs/", config_name="defaults")
 def show_parameters(opt: DictConfig) -> None:
+    '''(自定义)查看 model.parameters() 属性.'''
     opt = utils.parse_args(opt)
     utils.show_model_parameters(opt)
 
+
 if __name__ == "__main__":
     my_main()
+
