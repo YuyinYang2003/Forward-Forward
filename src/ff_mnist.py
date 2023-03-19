fdcfa1b67b0c30d2aad4232c211c10615c5078f0
YuyinYang <120449838+YuyinYang2003@users.noreply.github.com>
2023-03-19 12:07:48 +0800

    Update ff_mnist.py

--- src/ff_mnist.py
+++ src/ff_mnist.py
@@ -3,16 +3,18 @@ import torch
 
 from src import utils
 
-
-class FF_MNIST(torch.utils.data.Dataset):
-    def __init__(self, opt, partition, num_classes=10):
+# 最后需要把类名改为: FF_Dataset
+class FF_Dataset(torch.utils.data.Dataset):
+    '''在 torchvision 提供的数据集基础上, 新增了获取 pos/neg/neutral sample 的方法.'''
+    def __init__(self, opt, partition):
+        dataset = {'mnist': utils.get_MNIST_partition, 'cifar10': utils.get_CIFAR10_partition}
         self.opt = opt
-        self.mnist = utils.get_MNIST_partition(opt, partition)
-        self.num_classes = num_classes
-        self.uniform_label = torch.ones(self.num_classes) / self.num_classes    #对所有label权重相同的向量
+        self.dataset = dataset[self.opt.input.dataset](opt, partition)
+        self.num_classes = self.opt.input.num_classes
+        self.uniform_label = torch.ones(self.num_classes) / self.num_classes
 
     def __getitem__(self, index):
-        #输入index,输出数据集相应index的sample变化出pos/neg/neutral sample和对应的正确label
+        '''返回 inputs dict: 包含 pos/neg/neutral sample 和 labels dict: 包含 class_label'''
         pos_sample, neg_sample, neutral_sample, class_label = self._generate_sample(
             index
         )
@@ -26,20 +28,20 @@ class FF_MNIST(torch.utils.data.Dataset):
         return inputs, labels
 
     def __len__(self):
-        return len(self.mnist)
+        return len(self.dataset)
 
     def _get_pos_sample(self, sample, class_label):
-        #输入sample和对应的label,输出前十个元素对应了正确label的pos sample
+        '''把单个 sample 的第一行前10列替换为 one-hot vector.'''
+        # sample 是 ToTensor() 的返回值, 形状是 C x H x W, 故 pos_sample 的第 0 维是 Channels.
         one_hot_label = torch.nn.functional.one_hot(
             torch.tensor(class_label), num_classes=self.num_classes
-        )   #class_label位置为1,其余为0的长度为num_classes的向量
+        )
         pos_sample = sample.clone()
-        pos_sample[:, 0, : self.num_classes] = one_hot_label    #pos_sample形状是CxHxW,将第一行前num_classes个元素替换为对应label的one-hot vector
+        pos_sample[0, 0, : self.num_classes] = one_hot_label
         return pos_sample
 
     def _get_neg_sample(self, sample, class_label):
-        #输入sample和对应的label,输出前十个元素对应了错误label的pos sample
-        # Create randomly sampled one-hot label.
+        '''Create randomly sampled one-hot label.'''
         classes = list(range(self.num_classes))
         classes.remove(class_label)  # Remove true label from possible choices.
         wrong_class_label = np.random.choice(classes)
@@ -47,17 +49,16 @@ class FF_MNIST(torch.utils.data.Dataset):
             torch.tensor(wrong_class_label), num_classes=self.num_classes
         )
         neg_sample = sample.clone()
-        neg_sample[:, 0, : self.num_classes] = one_hot_label
+        neg_sample[0, 0, : self.num_classes] = one_hot_label
         return neg_sample
 
     def _get_neutral_sample(self, z):
-        #输入sample,输出前十个元素相同,且和为1,对所有label倾向相同的sample
-        z[:, 0, : self.num_classes] = self.uniform_label
+        z[0, 0, : self.num_classes] = self.uniform_label
         return z
 
     def _generate_sample(self, index):
-        # Get MNIST sample.
-        sample, class_label = self.mnist[index]
+        '''返回完整的 MNIST sample: 包含 pos/neg/neutral sample 以及 class_label'''
+        sample, class_label = self.dataset[index]
         pos_sample = self._get_pos_sample(sample, class_label)
         neg_sample = self._get_neg_sample(sample, class_label)
         neutral_sample = self._get_neutral_sample(sample)
